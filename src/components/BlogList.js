1b8ca87177adf3e746d81fecd9f020f7942c8846
Chhavi Garg <chhavu.g@gmail.com>
2022-10-16 21:35:38 +0530

    some changes

---
+++ src/components/BlogList.js
@@ -0,0 +1,26 @@
+import { Link } from 'react-router-dom';
+const BlogList = ({blogs}) => {
+
+    // const blogs = props.blogs;
+    // const title = props.title;
+
+    // console.log(props,blogs);
+    return (
+        <div className="blog-list">
+            {blogs.map((blog)=>(
+                <div className="blog-preview" key={blog.id}>
+                <Link to={`/blogs/${blog.id}`}>
+                    <h2>{ blog.title }</h2>
+                    <p>Written by { blog.author }</p>
+                    
+                </Link>
+                     {/* <h2>{blog.title}</h2>
+                     <p>Written by {blog.author}</p>
+                     <button onClick={()=>handleDelete(blog.id)}>Delete Blog</button> */}
+                </div>
+            ))}
+        </div>
+      );
+}
+ 
+export default BlogList ;
